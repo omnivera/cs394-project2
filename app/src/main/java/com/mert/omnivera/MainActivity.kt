64d2fd67a10745b2c222c30f3a5fccbfae70da88
omnivera <mert.dallar@ozu.edu.tr>
2021-12-25 16:40:22 +0300

    added new pages and adjusted them

--- app/src/main/java/com/mert/omnivera/MainActivity.kt
+++ app/src/main/java/com/mert/omnivera/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.mert.omnivera
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,4 +14,21 @@ class MainActivity : AppCompatActivity() {
 
         
     }
+
+
+    fun signInClicked(view : View) {
+
+
+
+
+
+    }
+
+    fun signUpClicked(view : View) {
+
+
+
+
+    }
+
 }
--- app/src/main/java/com/mert/omnivera/UploadActivity.kt
+++ app/src/main/java/com/mert/omnivera/UploadActivity.kt
@@ -1,11 +1,41 @@
 package com.mert.omnivera
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.Timestamp
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import java.util.*
 
 class UploadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload)
     }
+
+
+    fun imageViewClicked(view : View)  {
+
+
+
+    }
+
+    fun uploadClicked (view: View) {
+
+
+
+
+
+    }
+
+
 }

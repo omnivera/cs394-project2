6d4bca902dd4efd1777d9eefdf474eadd5e1a0b3
omnivera <mert.dallar@ozu.edu.tr>
2022-01-06 20:36:58 +0300

    created crypto API

--- app/src/main/java/com/mert/omnivera/service/CryptoAPI.kt
+++ app/src/main/java/com/mert/omnivera/service/CryptoAPI.kt
@@ -1,4 +1,11 @@
 package com.mert.omnivera.service
 
+import com.mert.omnivera.model.CryptoModel
+import io.reactivex.Observable
+import retrofit2.http.GET
+
 interface CryptoAPI {
+
+    @GET("omnivera/Cryptos/main/crypto.json?token=ARBUI7KYAFGBVUDXTBEVH7DB24TVI")
+    fun getData(): Observable<List<CryptoModel>>
 }

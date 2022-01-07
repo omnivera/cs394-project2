8bc17c637d8ac0c45138ca8055a3dbbd979f0c97
omnivera <mert.dallar@ozu.edu.tr>
2022-01-07 11:16:35 +0300

    visual design of crypto

--- app/src/main/java/com/mert/omnivera/CryptoActivity.kt
+++ app/src/main/java/com/mert/omnivera/CryptoActivity.kt
@@ -2,10 +2,41 @@ package com.mert.omnivera
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mert.omnivera.model.CryptoModel
+import com.mert.omnivera.service.CryptoAPI
+import io.reactivex.schedulers.Schedulers
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 class CryptoActivity : AppCompatActivity() {
+
+    private val BASE_URL = "https://raw.githubusercontent.com/"
+    private var cryptoModels: ArrayList<CryptoModel>? = null
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crypto)
     }
+
+
+
+    private fun loadData() {
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build().create(CryptoAPI::class.java)
+
+
+        
+
+
+
+
+    }
+
 }

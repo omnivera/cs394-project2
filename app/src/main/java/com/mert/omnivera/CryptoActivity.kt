ebb38d2fe12016b150c8240275761b2c800807d1
omnivera <mert.dallar@ozu.edu.tr>
2022-01-07 12:26:17 +0300

    adjusted crypto activity

--- app/src/main/java/com/mert/omnivera/CryptoActivity.kt
+++ app/src/main/java/com/mert/omnivera/CryptoActivity.kt
@@ -2,23 +2,41 @@ package com.mert.omnivera
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.mert.omnivera.adapter.CryptoRecyclerAdapter
 import com.mert.omnivera.model.CryptoModel
 import com.mert.omnivera.service.CryptoAPI
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_crypto.*
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
-class CryptoActivity : AppCompatActivity() {
+class CryptoActivity : AppCompatActivity(),CryptoRecyclerAdapter.Listener {
 
     private val BASE_URL = "https://raw.githubusercontent.com/"
     private var cryptoModels: ArrayList<CryptoModel>? = null
+    private var recyclerViewAdapter : CryptoRecyclerAdapter? = null
+    private var compositeDisposable: CompositeDisposable? = null
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crypto)
+
+
+
+        val layoutManager : RecyclerView.LayoutManager = LinearLayoutManager(this)
+        cryptoView.layoutManager = layoutManager
+
+        loadData()
+
+
     }
 
 
@@ -37,6 +55,26 @@ class CryptoActivity : AppCompatActivity() {
 
 
 
+
+    }
+
+    override fun onItemClick(cryptoModel: CryptoModel) {
+        Toast.makeText(this,"Clicked : ${cryptoModel.currency}", Toast.LENGTH_LONG ).show()
+    }
+
+    private fun handleResponse(cryptoList : List<CryptoModel>){
+        cryptoModels = ArrayList(cryptoList)
+
+        cryptoModels?.let {
+            recyclerViewAdapter = CryptoRecyclerAdapter(it,this)
+            cryptoView.adapter = recyclerViewAdapter
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        compositeDisposable?.clear()
     }
 
 }
